4ee31b2d7763ecf84b211ad08f16650bba63c397
leviblackark <mrgnpnn@gmail.com>
2024-09-15 23:14:13 +0100

    Created the Rock Paper Scissors

--- Project 4 Rock Paper Scissors.py
+++ Project 4 Rock Paper Scissors.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -29,6 +30,9 @@ scissors = '''
 game_images = [rock, paper, scissors]
 # print(game_images[1])
 
+# Rock wins against Scissors 0 wins 2, Rock against Rock 0 draw 0
+# Scissors  wins against Paper 2 to 0, Scissors against Scissors 2 draw 2
+# Paper wins against Rock 1 wins 0, Paper against Paper 1 draw 1
 user_choice = int(input("What do you choose? Type:"
                         "\n0 for Rock, "
                         "\n1 for Paper,"
@@ -36,4 +40,35 @@ user_choice = int(input("What do you choose? Type:"
 if user_choice < 0 or user_choice >= 3:
     print("The number you have typed is invalid, You lose")
 else:
-    print("Works")
+    print("Your Choice 🤤")
+    # Print the corresponding images to the numbers
+    if user_choice == 0:
+        print(game_images[0])
+    elif user_choice == 1:
+        print(game_images[1])
+    else:
+        print(game_images[2])
+
+    print("Computer's Choice 😎")
+    # Create random number and use that for the machines choice
+    number_index = random.randint(0, 2)
+    print(game_images[number_index])
+
+    # Compair Results to decide a winner
+    if number_index > user_choice:
+        print("You Win!")
+    elif user_choice == 1 and number_index == 2:
+        print("You Lose!")
+    elif user_choice == 2 and number_index == 0:
+        print("You Lose!")
+    elif user_choice == 0 and number_index == 1:
+        print("You Lose!")
+    elif user_choice > number_index:
+        print("You Win!")
+    elif user_choice == number_index:
+        print("You Draw!")
+    else:
+        print("You Lose!")
+
+
+
