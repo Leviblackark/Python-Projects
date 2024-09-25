2285c198ea0a0c1b40ee02488a947a123a4e43b1
leviblackark <mrgnpnn@gmail.com>
2024-09-25 16:38:51 +0100

    I was working on creating a function and practicing keyword arguments and positional arguments. This tests for letters in "true" and "love" across the whole name and then combines them in to a number. the inputs value is passed in to variable used by the function so that the result can change

---
+++ Test Example #1 Functions With Inputs.py
@@ -0,0 +1,30 @@
+def calculate_love_score(name1, name2):
+    name = (name1 + name2).lower()
+
+    t = name.count("t")
+    r = name.count("r")
+    u = name.count("u")
+    e = name.count("e")
+
+    true = t + r + u + e
+
+    l = name.count("l")
+    o = name.count("o")
+    v = name.count("v")
+    e = name.count("e")
+
+    love = l + o + v + e
+
+    score = str(true) + str(love)
+
+    print(score)
+
+
+print("Calculate your love score!")
+
+first_name = input("Type your name: ")
+second_name = input("Type your match: ")
+
+
+print(f"{first_name} and {second_name} you have a Grand Match of : ")
+calculate_love_score(name1 = first_name, name2 = second_name)
