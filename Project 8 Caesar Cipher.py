5d917f87c070e034b69872e06a76e95a88ade44a
leviblackark <mrgnpnn@gmail.com>
2024-09-28 14:11:57 +0100

    Combine encoding and decoding. The only difference is learning out to flip + to a -. So it's done in less lines of code

--- Project 8 Caesar Cipher.py
+++ Project 8 Caesar Cipher.py
@@ -1,13 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(original_text, shift_amount):
+def caesar(original_text, shift_amount, encode_decode):
     code = ""
+    # 3 + ( 5 * -1) is the same as 3 - 5
+    if encode_decode == "decode":
+        shift_amount *= -1
 
     for instance in original_text:
         # find the index of instance within alphabet
@@ -18,7 +17,42 @@ def encrypt(original_text, shift_amount):
         position %= len(alphabet)
         # call the new location of that letter and save within code
         code += alphabet[position]
-    print(f"Here is the encoded result: {code}")
+
+    print(f"Here is the {encode_decode}d result: {code}")
 
 
-encrypt(original_text=text, shift_amount=shift)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+caesar(original_text=text, shift_amount=shift, encode_decode=direction)
+
+# def encrypt(original_text, shift_amount):
+#     code = ""
+#
+#     for instance in original_text:
+#         # find the index of instance within alphabet
+#         location_number = alphabet.index(instance)
+#         # current index of current letter + the amount you want to shift
+#         position = location_number + shift_amount
+#         # Keeps within the range
+#         position %= len(alphabet)
+#         # call the new location of that letter and save within code
+#         code += alphabet[position]
+#     print(f"Here is the encoded result: {code}")
+#
+#
+# def decrypt(original_text, shift_amount):
+#     decode = ""
+#     for instance in original_text:
+#         # combined the index and the amount to shift
+#         shifted_index = alphabet.index(instance) - shift_amount
+#         shifted_index %= len(alphabet)
+#         decode += alphabet[shifted_index]
+#
+#     print(f"Here is the decoded result: {decode}")
+#
+#
+# encrypt(original_text=text, shift_amount=shift)
+# decrypt(original_text=text, shift_amount=shift)
+
