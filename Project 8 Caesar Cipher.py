a4d36e7dafa760c856cef1e065c74cfb6e1e9cea
leviblackark <mrgnpnn@gmail.com>
2024-09-29 09:09:28 +0100

    used a while loop to restart the program and added whatever wasn't in the alphabet to the rest as it went through the code

--- Project 8 Caesar Cipher.py
+++ Project 8 Caesar Cipher.py
@@ -1,3 +1,5 @@
+from project8art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
@@ -9,20 +11,30 @@ def caesar(original_text, shift_amount, encode_decode):
         shift_amount *= -1
 
     for instance in original_text:
-        # find the index of instance within alphabet
-        location_number = alphabet.index(instance)
-        # current index of current letter + the amount you want to shift
-        position = location_number + shift_amount
-        # Keeps within the range
-        position %= len(alphabet)
-        # call the new location of that letter and save within code
-        code += alphabet[position]
+        if instance not in alphabet:
+            code += instance
+        else:
+            # find the index of instance within alphabet
+            location_number = alphabet.index(instance)
+            # current index of current letter + the amount you want to shift
+            position = location_number + shift_amount
+            # Keeps within the range
+            position %= len(alphabet)
+            # call the new location of that letter and save within code
+            code += alphabet[position]
 
     print(f"Here is the {encode_decode}d result: {code}")
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-caesar(original_text=text, shift_amount=shift, encode_decode=direction)
+    caesar(original_text=text, shift_amount=shift, encode_decode=direction)
+
+    restart_program = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart_program == "no":
+        restart = False
+        print("Goodbye!")
