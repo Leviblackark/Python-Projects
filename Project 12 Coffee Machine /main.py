ec5a84b451dbfe4a063e8cd3b16c60f05dc6ef6c
leviblackark <mrgnpnn@gmail.com>
2024-11-03 21:51:54 +0000

    Check that there is enough resources to make the coffe

--- Project 12 Coffee Machine /main.py
+++ Project 12 Coffee Machine /main.py
@@ -26,8 +26,9 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
+    # 300 200 100
+    "water": 100,
+    "milk": 100,
     "coffee": 100,
 }
 
@@ -43,6 +44,24 @@ def check_resources(value, pot):
     account = "Money: $" + str(pot)
 
     print(f"{water}\n{milk}\n{coffee}\n{account}")
+
+
+def compare_resources(ingredients, machine_hold):
+    """Checks if resource is available and if not tells the user what's out"""
+    out_of = []
+
+    for key_word in machine_hold:
+        # Each key word inside resources loop and checked against the MENU if inside
+        if key_word in ingredients:
+            # print("Yes")
+            if machine_hold[key_word] < ingredients[key_word]:
+                out_of.append(key_word)
+    # To only print one statement for what's out saved outside the loop
+    if len(out_of) > 1:
+        convert = " and ".join(out_of)
+        print(f"Sorry there is not enough {convert}.")
+    else:
+        print(f"Sorry there is not enough {out_of[0]}.")
 
 
 # TODO 1: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
@@ -68,19 +87,26 @@ while coffee_machine:
     elif response == "off":
         coffee_machine = False
 
-# TODO 4: Check resources Sufficient? When the user chooses a drink, the program should check if there are enough
-#  resources to make that drink
+    # TODO 4: Check resources Sufficient? When the user chooses a drink, the program should check if there are enough
+    #  resources to make that drink
+    # Compare what's in the resources to ingredients needed, if less, then tell the user "Sorry not enough" and reprompt
+    #  Access for entered order. “Sorry there is not enough water.” and for each one
 
-# Compare what's in the resources to ingredients needed, if less, then tell the user "Sorry not enough" and reprompt
-#  Access for entered order. “Sorry there is not enough water.” and for each one
+    elif  response == "latte":
+        # MENU["latte"]["ingredients"]
+        compare_resources(ingredients=MENU["latte"]["ingredients"], machine_hold=resources)
+    elif response == "espresso":
+        compare_resources(ingredients=MENU["espresso"]["ingredients"], machine_hold=resources)
+    elif response == "cappuccino":
+        compare_resources(ingredients=MENU["cappuccino"]["ingredients"], machine_hold=resources)
 
-# TODO 5: Process Coins. If there are sufficient resources to make the drink selected, then the program should
-#  prompt the user to insert coins.
+    # TODO 5: Process Coins. If there are sufficient resources to make the drink selected, then the program should
+    #  prompt the user to insert coins.
 
-# Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+    # Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
 
-# Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-#  pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+    # Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+    #  pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
 
 # TODO 6: Check Transaction Successful?
 #  Check that the user has inserted enough money to purchase the drink they selected.
